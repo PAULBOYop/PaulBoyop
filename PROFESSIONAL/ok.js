6bbccbe3a56497512d46e8ac5fff0a31713ebd95
PAULBOYop <99779607+PAULBOYop@users.noreply.github.com>
2022-02-18 12:39:08 +0545

    Add files via upload

---
+++ PROFESSIONAL/ok.js
@@ -0,0 +1,9 @@
+function ro(){
+    window.open("https://www.facebook.com")
+}
+function rom(){
+    window.open("https://www.twitter.com")
+}
+function ram(){
+    window.open("https://www.instagram.com")
+}
